13bbd13546a523b28698ccdf57bd3c82c01dbe47
iwmh <hiroshiplusone@gmail.com>
2020-10-19 21:41:42 +0900

    replaced the package name with env value

--- main.go
+++ main.go
@@ -28,8 +28,9 @@ func main() {
 func hello(c echo.Context) error {
 	code := c.QueryParam("code")
 	state := c.QueryParam("state")
+	packageName := os.Getenv("PACKAGE")
 	url := "https://wear.googleapis.com/3p_auth/app.html?full_suffix_from_redirect=" +
-		"com.example.spotifywearapp" + "?code=" + code + "&state=" + state
+		packageName + "?code=" + code + "&state=" + state
 	return c.Redirect(http.StatusMovedPermanently, url)
 }
 
